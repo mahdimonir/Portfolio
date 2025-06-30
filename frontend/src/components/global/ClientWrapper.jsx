ddbdeca65031bcac69c320e05b40746a3b30d71e
mahdimonir <mahdimoniruzzaman@gmail.com>
2025-07-01 00:43:53 +0600

    Add ClientWrapper and Navbar components for enhanced layout and navigation

---
+++ frontend/src/components/global/ClientWrapper.jsx
@@ -0,0 +1,29 @@
+"use client";
+
+import Navbar from "@/components/global/Navbar";
+import ThemeProvider from "@/components/ThemeProvider";
+import { Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import DashboardBubble from "./DashboardBubble";
+import SecretLoginTrigger from "./SecretLoginTrigger";
+
+const queryClient = new QueryClient();
+
+export function ClientWrapper({ children }) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <ThemeProvider>
+          <div id="root" className="bg-background">
+            {children}
+            <Sonner />
+            <Navbar />
+            <DashboardBubble />
+            <SecretLoginTrigger />
+          </div>
+        </ThemeProvider>
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
