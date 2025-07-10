54a436f8cde6cd11dd832e2fcab6648c600d69d0
Moniruzzaman Mahdi <159605851+mahdimonir@users.noreply.github.com>
2025-07-10 15:07:20 +0600

    Update user.js

--- frontend/src/data/user.js
+++ frontend/src/data/user.js
@@ -4,14 +4,30 @@ export const User = {
   password: "",
   fullName: "Moniruzzaman Mahdi",
   avatar: "https://avatars.githubusercontent.com/u/159605851?v=4",
-  tagLine: "Full Stack MERN Developer & UI/UX Designer",
+  tagLines: [
+    "MERN Stack Developer",
+    "Full Stack JavaScript Developer",
+    "React & Node.js Specialist",
+    "MongoDB & Express.js Expert",
+    "Building Scalable Web Applications",
+    "JavaScript Problem Solver",
+    "API Development Specialist",
+    "Database Design Expert",
+    "Modern Web Development Enthusiast",
+  ],
   about:
     "Passionate about creating beautiful, functional web applications using the MERN stack. I love turning complex problems into simple, elegant solutions.",
+  contact: {
+    email: "mahdimoniruzzaman@gmail.com",
+    phone: "+8801876689921",
+    location: "Chattogram, Bangladesh",
+  },
   socialLinks: {
     github: "https://github.com/mahdimonir",
     linkedin: "https://www.linkedin.com/in/moniruzzaman-mahdi/",
     twitter: "https://x.com/Mahdimonir2004",
     email: "mahdimoniruzzaman@gmail.com",
   },
-  resume: "https://github.com/mahdimonir",
+  resume:
+    "https://res.cloudinary.com/devmahdi/raw/upload/v1751995157/Portfolio/RESUMES/MoniruzzamanMahdi_Resume_1.pdf",
 };
